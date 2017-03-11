8860c28748a3060505a88394a90e8f5084cca0b0
andrey <andkr5150@gmail.com>
2017-03-11 10:46:04 +0200

    correct edit.php

--- controller/edit.php
+++ controller/edit.php
@@ -1,18 +1,21 @@
 <?php
 require_once '../model/model.php';
 
-if (!empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['created_at']) && !empty($_POST['id'])){
-    update($_POST['name'], $_POST['description'], $_POST['created_at'], $_POST['id']);
+$ss = $_POST;
+var_dump($ss);
+
+
+if ( !empty($_POST['id_ed']) && !empty($_POST['name_ed']) && !empty($_POST['description_ed']) && !empty($_POST['created_at_ed'])){
+    update($_POST['id_ed'], $_POST['name_ed'], $_POST['description_ed'], $_POST['created_at']);
 
     header("location:edit.php?id=".$id);
 }
 
 $str = edit($_GET['id']);
-
 require_once '../view/editForm.php';
 
-if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['created_at'])) {
-    update($_POST['name'], $_POST['description'], $_POST['created_at'], $_POST['id']);
+if (isset($_POST['name_ed']) && isset($_POST['description_ed']) && isset($_POST['created_at_ed'])) {
+    update($_POST['id_ed'], $_POST['name_ed'], $_POST['description_ed'], $_POST['created_at_ed']);
 
     header("location:index.php");
 }
