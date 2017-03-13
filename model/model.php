e499ef23a63846e9812cf87962ad566fa4095c17
andrey <andkr5150@gmail.com>
2017-03-13 22:22:37 +0200

    modify model

--- model/model.php
+++ model/model.php
@@ -15,17 +15,17 @@ function connect()
 
 function view(){
     $dbh = connect();
-    $sth = $dbh ->prepare('SELECT * FROM form');
-    $sth -> execute();
+    $sth = $dbh->prepare('SELECT * FROM form');
+    $sth->execute();
     $stat = $sth->fetchAll();
     return $stat;
 }
 
 function edit($id){
     $dbh = connect();
-    $sth = $dbh ->prepare('SELECT * FROM form WHERE id=:id');
-    $sth -> bindValue(':id', $id, PDO::PARAM_INT);
-    $sth -> execute();
+    $sth = $dbh->prepare('SELECT * FROM form WHERE id=:id');
+    $sth->bindValue(':id', $id, PDO::PARAM_INT);
+    $sth->execute();
     $res = $sth->fetch();
     return $res;
 }
@@ -33,28 +33,27 @@ function edit($id){
 function update($id, $name,$description,$created_at){
     $dbh = connect();
     $sth = $dbh ->prepare('UPDATE form SET name=:name, discription =:description, created_at=:created_at WHERE id=:id');
-    $sth -> bindValue(':id', $id, PDO::PARAM_INT);
-    $sth -> bindValue(':name', $name, PDO::PARAM_STR);
-    $sth -> bindValue(':description', $description, PDO::PARAM_STR);
-    $sth -> bindValue(':created_at', $created_at, PDO::PARAM_STR);
-    $sth -> execute();
+    $sth->bindValue(':id', $id, PDO::PARAM_INT);
+    $sth->bindValue(':name', $name, PDO::PARAM_STR);
+    $sth->bindValue(':description', $description, PDO::PARAM_STR);
+    $sth->bindValue(':created_at', $created_at, PDO::PARAM_STR);
+    $sth->execute();
 }
 
 function add($name, $description, $created_at){
     $dbh = connect();
     $sth = $dbh ->prepare('INSERT INTO form (id, name, discription, created_at) VALUES (0, :name, :description, :created_at)');
-    $sth -> bindValue(':name', $name, PDO::PARAM_STR);
-    $sth -> bindValue(':description', $description, PDO::PARAM_STR);
-    $sth -> bindValue(':created_at', $created_at, PDO::PARAM_STR);
-    $sth -> execute();
-    $id = $dbh ->lastInsertId();
+    $sth->bindValue(':name', $name, PDO::PARAM_STR);
+    $sth->bindValue(':description', $description, PDO::PARAM_STR);
+    $sth->bindValue(':created_at', $created_at, PDO::PARAM_STR);
+    $sth->execute();
+    $id = $dbh->lastInsertId();
     return $id;
 }
 
 function delete($id){
     $dbh = connect();
-    $sth = $dbh ->prepare('DELETE FROM form WHERE id=:id');
-    $sth -> bindValue(':id', $id, PDO::PARAM_INT);
-    $sth -> execute();
+    $sth = $dbh->prepare('DELETE FROM form WHERE id=:id');
+    $sth->bindValue(':id', $id, PDO::PARAM_INT);
+    $sth->execute();
 }
-
